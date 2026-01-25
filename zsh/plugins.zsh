273b02815dc3a2c53877987839c74f1fe657f2e1
wenqiang.jiang <wenqiang.jiang@dmall.com>
2026-01-25 11:26:27 +0800

    zsh config

---
+++ zsh/plugins.zsh
@@ -0,0 +1,9 @@
+# edit-command-line
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^X^E' edit-command-line
+
+# ZOXIDE 
+if command -v zoxide >/dev/null 2>&1; then
+    eval "$(zoxide init zsh)"
+fi
