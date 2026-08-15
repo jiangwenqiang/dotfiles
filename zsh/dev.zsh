1794044ee3a03c79ce75a68c43c3ffb4175d48ad
jiangwenqiang <chinesejwq@163.com>
2026-08-15 08:25:13 +0800

    Update dev.zsh

--- zsh/dev.zsh
+++ zsh/dev.zsh
@@ -4,8 +4,8 @@ export SDKMAN_DIR="$HOME/.sdkman"
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # pnpm
 export PNPM_HOME="/Users/hermes/Library/pnpm"
